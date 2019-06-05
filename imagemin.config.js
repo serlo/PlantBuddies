2cdad5ff6c37ef84c36390c8299f0feec9bfbb41
elpoto <1258870+elbotho@users.noreply.github.com>
2019-06-05 13:54:40 +0200

    add _redirects and fix https error

--- imagemin.config.js
+++ imagemin.config.js
@@ -1,12 +1,12 @@
 module.exports = {
   "gifsicle": { "optimizationLevel": 2, "interlaced": false, "colors": 10 },
-  "mozjpeg": { "progressive": true, "quality": 10 },
-  "pngquant": { "quality": 10 },
+  "mozjpeg": { "progressive": true, "quality": 7 },
+  "pngquant": { "quality": 8 },
   "svgo": {
     "plugins": [
       { "removeViewBox": false },
       { "cleanupIDs": true },
     ]
   },
-  "webp": { "quality": 10 }
+  "webp": { "quality": 8 }
 }
