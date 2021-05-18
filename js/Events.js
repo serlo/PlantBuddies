b6c112307e23ea941084f21258dcab697492b6f5
Botho <1258870+elbotho@users.noreply.github.com>
2021-05-18 12:02:16 +0200

    updates and remove console logs

--- js/Events.js
+++ js/Events.js
@@ -1,321 +1,324 @@
-import Navigo from 'navigo'
+import Navigo from "navigo";
 
-import getHeaderContent from './header'
-import getFooterContent from './footer'
+import getHeaderContent from "./header";
+import getFooterContent from "./footer";
 
-var router = new Navigo('/');
-window.router = router
-window.gLang = 'en'
-window.gPlant = ''
-
+var router = new Navigo("/");
+window.router = router;
+window.gLang = "en";
+window.gPlant = "";
 
 export default function Events() {
+  var cache = {};
+  var currentRoute, lastRoute;
 
-	var cache = {}
-	var currentRoute, lastRoute
+  this.init = function () {
+    this.initKeyEvents();
 
-	this.init = function () {
+    // setupLangMenu(gLang)
 
-		this.initKeyEvents();
+    setupRouter();
+  };
 
-		// setupLangMenu(gLang)
+  function buildCache() {
+    if (!cache["front"]) cache["front"] = buildFrontPage(true);
+    if (!cache["plants"]) cache["plants"] = gPlants.buildPlantList();
+  }
 
-		setupRouter();
-	}
+  function setGlobals(p, pageName, callback) {
+    gPage = pageName;
+    if (typeof p === undefined) gLang = "en";
+    else gLang = p.lang.length !== 2 ? "en" : p.lang;
+    setLanguage(callback);
+  }
 
-	function buildCache() {
-		if (!cache['front']) cache['front'] = buildFrontPage(true);
-		if (!cache['plants']) cache['plants'] = gPlants.buildPlantList()
-	}
+  var buildPage = function () {
+    document.title = gLangMain.meta.title;
+    document
+      .querySelector("meta[name=description]")
+      .setAttribute("content", gLangMain.meta.desc);
+    document
+      .querySelector("meta[property=og\\:title]")
+      .setAttribute("content", gLangMain.meta.title);
+    document
+      .querySelector("#logo img")
+      .setAttribute("alt", gLangMain.meta.title + " Logo");
 
-	function setGlobals(p, pageName, callback) {
-		gPage = pageName;
-		if (typeof p === undefined) gLang = 'en'
-		else gLang = p.lang.length !== 2 ? 'en' : p.lang
-		setLanguage(callback)
-	}
+    // import('./header.js').then(function(e){
+    // 	document.querySelector('header > nav').innerHTML = e
+    // 	setupLangMenu(gLang)
+    // })
 
-	var buildPage = function () {
-		document.title = gLangMain.meta.title
-		document.querySelector('meta[name=description]').setAttribute('content', gLangMain.meta.desc)
-		document.querySelector('meta[property=og\\:title]').setAttribute('content', gLangMain.meta.title)
-		document.querySelector('#logo img').setAttribute('alt', gLangMain.meta.title + ' Logo')
+    document.querySelector("header > nav").innerHTML =
+      getHeaderContent(gLangMain);
+    setupLangMenu(gLang);
+    document.getElementsByTagName("footer")[0].innerHTML =
+      getFooterContent(gLangMain);
 
-		// import('./header.js').then(function(e){
-		// 	document.querySelector('header > nav').innerHTML = e
-		// 	setupLangMenu(gLang)
-		// })
+    setTimeout(buildCache, 200);
 
-		document.querySelector('header > nav').innerHTML = getHeaderContent(gLangMain)
-		setupLangMenu(gLang)
-		document.getElementsByTagName('footer')[0].innerHTML = getFooterContent(gLangMain)
+    // import('./footer'.js).then(function(e){
+    // 	document.getElementsByTagName('footer')[0].innerHTML = e
+    // 	setTimeout(buildCache,200)
+    // })
+  };
 
-		setTimeout(buildCache, 200)
+  var setLanguage = function (callback) {
+    // console.log("setLang:" + gLang)
+    if (gLang === "en")
+      import("../data/lang/en/main").then(function () {
+        buildPage();
+        callback();
+      });
+    else if (gLang === "de")
+      import("../data/lang/de/main").then(function () {
+        buildPage();
+        callback();
+      });
+    else return false;
+    document.documentElement.setAttribute("lang", gLang);
+    document.cookie = "nf_lang=" + gLang;
+  };
 
-		// import('./footer'.js).then(function(e){
-		// 	document.getElementsByTagName('footer')[0].innerHTML = e
-		// 	setTimeout(buildCache,200)
-		// })
+  function setupRouter() {
+    // cache = {}
+    router
+      .on({
+        ":lang/🌿": function (p) {
+          setGlobals(p, "plants", loadPlantList);
+        },
+        ":lang/🌿/:id": function (p) {
+          gPlant = p.id;
+          setGlobals(p, "plant", loadPlantPage);
+          // console.log("plant.id: " + gPlant)
+        },
+        ":lang": function (p) {
+          // console.log("landing?!")
+          setGlobals(p, "front", gEvents.loadStartPage);
+          //TODO: Check if changes, otherwise dont render again
+        },
+        "*": function (p) {
+          setGlobals(p, "front", gEvents.loadStartPage);
+          // (); scrollToPos(0);
+        },
+      })
+      .resolve();
 
-	}
+    // console.log("gPage: " + gPage)
+    // console.log("gLang: " + gLang)
+    document.body.className = gPage; //for first load
 
-	var setLanguage = function (callback) {
-		console.log("setLang:" + gLang)
-		if (gLang === 'en') import('../data/lang/en/main').then(function () {
-			buildPage()
-			callback()
-		})
-		else if (gLang === 'de') import('../data/lang/de/main').then(function () {
-			buildPage()
-			callback()
-		})
-		else return false
-		document.documentElement.setAttribute('lang', gLang)
-		document.cookie = "nf_lang=" + gLang;
-	}
+    router.getLinkPath = function (link) {
+      // var href = gLang === 'en' ? link.getAttribute('href') : gLang + '/' + link.getAttribute('href');
+      var href = link.getAttribute("href");
+      if (href[0] === "/") href = href.replace("/", "./");
+      return href;
+    };
+    // router.getLinkPath = function(link){
+    // 	var location = link.getAttribute('href');
+    // 	console.log(location)
+    // 	console.log(link)
+    // 	  e.preventDefault();
+    // 	// router.navigate(location);
+    // 	return false
 
-	function setupRouter() {
-		// cache = {}
-		router
-			.on({
-				':lang/🌿': function (p) {
-					setGlobals(p, 'plants', loadPlantList)
-				},
-				':lang/🌿/:id': function (p) {
-					gPlant = p.id
-					setGlobals(p, 'plant', loadPlantPage)
-					console.log("plant.id: " + gPlant)
-				},
-				':lang': function (p) {
-					console.log("landing?!")
-					setGlobals(p, 'front', gEvents.loadStartPage)
-					//TODO: Check if changes, otherwise dont render again
-				},
-				'*': function (p) {
-					setGlobals(p, 'front', gEvents.loadStartPage)
-					// (); scrollToPos(0);
-				}
-			})
-			.resolve();
+    // }
 
-		console.log("gPage: " + gPage)
-		console.log("gLang: " + gLang)
-		document.body.className = gPage //for first load
+    router.hooks({
+      before: function (done, params) {
+        // console.log(params)x
+        // console.log("router before")
+        // console.log(done)
+        // console.log(params)
+        // if(!currentRoute) currentRoute = window.location.href
+        // console.log(window.location.href)
+        // lastRoute = currentRoute;
+        // currentRoute = router.lastRouteResolved()
+        done();
+      },
+      after: function (params) {},
+    });
+  }
 
-		router.getLinkPath = function (link) {
-			// var href = gLang === 'en' ? link.getAttribute('href') : gLang + '/' + link.getAttribute('href');
-			var href = link.getAttribute('href');
-			if (href[0] === '/') href = href.replace('/', './')
-			return href;
-		}
-		// router.getLinkPath = function(link){
-		// 	var location = link.getAttribute('href');
-		// 	console.log(location)
-		// 	console.log(link)
-		// 	  e.preventDefault();
-		// 	// router.navigate(location);
-		// 	return false
+  var loadPlantPage = function () {
+    var plantObj = gPlantDataLang[gPlant];
+    if (plantObj === undefined) {
+      //find in language
+      for (var o in gPlantDataLang) {
+        var slug = toSlug(gPlantDataLang[o].name);
+        if (slug !== gPlant) continue;
+        //success
+        gPlant = o;
+        loadPlantPage();
+        return false;
+      }
+      router.navigate("");
+      return false;
+    }
+    gPlants.load(plantObj);
+    scrollToPos(0);
+  };
 
-		// }
+  var setupLangMenu = function () {
+    var menu = document.getElementById("lang-select");
+    menu.addEventListener("change", onLangMenuChange, false);
+    menu.value = "_change";
+    // setLanguage(gLang)
+  };
 
-		router.hooks({
-			before: function (done, params) {
-				// console.log(params)x
-				// console.log("router before")
-				// console.log(done)
-				// console.log(params)
-				// if(!currentRoute) currentRoute = window.location.href
-				// console.log(window.location.href)
-				// lastRoute = currentRoute;
-				// currentRoute = router.lastRouteResolved()
-				done()
-			},
-			after: function (params) {
-			}
-		});
+  var onLangMenuChange = function (e) {
+    var lang = e.target.value;
+    if (lang !== "en" && lang !== "de") return false;
+    if (lang === "en") lang = "";
+    window.location.href = "/" + lang;
+  };
 
-	}
+  var loadPlantList = function () {
+    if (cache["plants"]) gPlants.reload(cache["plants"]);
+    else {
+      cache["plants"] = gPlants.buildPlantList();
+      gPlants.reload(cache["plants"]);
+    }
+  };
 
-	var loadPlantPage = function () {
-		var plantObj = gPlantDataLang[gPlant];
-		if (plantObj === undefined) {
-			//find in language
-			for (var o in gPlantDataLang) {
-				var slug = toSlug(gPlantDataLang[o].name)
-				if (slug !== gPlant) continue
-				//success
-				gPlant = o
-				loadPlantPage();
-				return false;
-			}
-			router.navigate('');
-			return false
-		}
-		gPlants.load(plantObj);
-		scrollToPos(0);
-	}
+  this.loadStartPage = function (keepHash) {
+    //if (!keepHash) router.navigate('')
+    // gInput.clearInput()
+    if (cache["front"]) gPlants.reload(cache["front"]);
+    else {
+      buildFrontPage();
+    }
+  };
 
-	var setupLangMenu = function () {
-		var menu = document.getElementById('lang-select')
-		menu.addEventListener('change', onLangMenuChange, false)
-		menu.value = '_change';
-		// setLanguage(gLang)
-	}
+  var buildFrontPage = function (preload) {
+    import("./frontpage").then(function (e) {
+      cache["front"] = e(gLangMain);
+      if (!preload) gEvents.loadStartPage();
+    });
+  };
 
-	var onLangMenuChange = function (e) {
-		var lang = e.target.value
-		if (lang !== 'en' && lang !== 'de') return false;
-		if (lang === 'en') lang = '';
-		window.location.href = '/' + lang;
-	}
+  var toTop = function () {
+    scrollToPos(0);
+  };
 
-	var loadPlantList = function () {
-		if (cache['plants']) gPlants.reload(cache['plants'])
-		else {
-			cache['plants'] = gPlants.buildPlantList()
-			gPlants.reload(cache['plants']);
-		}
-	}
+  var scrollToElem = function (selector) {
+    var checkExist = setInterval(function () {
+      console.log(document.querySelector(selector));
+      if (document.querySelector(selector)) {
+        clearInterval(checkExist);
+        window.scrollTo({
+          top: document.querySelector(selector).offsetTop,
+          behavior: "smooth",
+        });
+      }
+    }, 50); // check every 50ms
+  };
 
-	this.loadStartPage = function (keepHash) {
-		//if (!keepHash) router.navigate('')
-		// gInput.clearInput()
-		if (cache['front']) gPlants.reload(cache['front'])
-		else {
-			buildFrontPage()
-		}
-	}
+  var scrollToPos = function (pos) {
+    window.scrollTo({
+      top: pos,
+      behavior: "smooth",
+    });
+  };
 
+  this.beforeReload = function () {
+    document.body.onclick = function () {};
+  };
 
-	var buildFrontPage = function (preload) {
-		import('./frontpage').then(function (e) {
-			cache['front'] = e(gLangMain);
-			if (!preload) gEvents.loadStartPage()
-		})
-	}
+  this.afterReload = function () {
+    document.body.className = gPage;
+    router.updatePageLinks();
+    if (gPage === "plants") gInput.findInput();
+    if (gPage === "front") decodeMail();
+    document.activeElement.blur();
+    scrollOrFocusInput();
+    document.dispatchEvent(new Event("prerender-trigger"));
+    //
+  };
 
-	var toTop = function () {
-		scrollToPos(0);
-	}
+  var scrollOrFocusInput = function () {
+    if (gPage !== "plants") return false;
+    if (!gInput.input) return false;
+    if (window.innerHeight > 700) gInput.input.focus();
+    else {
+      setTimeout(function () {
+        window.scroll({ top: gInput.input.offsetTop - 20 });
+      }, 50);
+    }
+  };
 
-	var scrollToElem = function (selector) {
-		var checkExist = setInterval(function () {
-			console.log(document.querySelector(selector))
-			if (document.querySelector(selector)) {
-				clearInterval(checkExist);
-				window.scrollTo({
-					top: document.querySelector(selector).offsetTop,
-					behavior: 'smooth'
-				});
-			}
-		}, 50); // check every 50ms
-	}
+  this.initKeyEvents = function () {
+    //list and input set in plantsFilter
 
-	var scrollToPos = function (pos) {
-		window.scrollTo({
-			top: pos,
-			behavior: 'smooth'
-		});
-	}
+    document.addEventListener("keydown", onKeyDown, false);
+  };
 
-	this.beforeReload = function () {
-		document.body.onclick = function () {
+  //TODO: INPUT AND FILTER EVENTS
+  function onKeyDown(e) {
+    const onInput = e.target.tagName.toLowerCase() === "input";
 
-		}
-	}
+    if (gPage === "plant" && e.keyCode === 27) router.navigate("🌿");
+    if (gPage === "plant" && e.keyCode === 8) window.history.back();
+    if (gPage !== "plants") return true;
 
-	this.afterReload = function () {
-		document.body.className = gPage
-		router.updatePageLinks()
-		if (gPage === 'plants') gInput.findInput();
-		if (gPage === 'front') decodeMail();
-		document.activeElement.blur();
-		scrollOrFocusInput()
-		document.dispatchEvent(new Event('prerender-trigger'));
-		//
-	}
+    if (
+      !onInput &&
+      e.keyCode >= 65 &&
+      e.keyCode <= 90 &&
+      !e.altKey &&
+      !e.ctrlKey &&
+      !e.metaKey
+    ) {
+      gInput.input.focus();
+      return true;
+    }
 
-	var scrollOrFocusInput = function () {
-		if (gPage !== 'plants') return false
-		if (!gInput.input) return false
-		if (window.innerHeight > 700) gInput.input.focus()
-		else {
-			setTimeout(function () {
-				window.scroll({ top: gInput.input.offsetTop - 20 })
-			}, 50)
-		}
-	}
+    switch (e.keyCode) {
+      case 27: //esc
+        gInput.setFilter("");
+        if (gInput.input) gInput.input.blur();
+        break;
 
-	this.initKeyEvents = function () {
-		//list and input set in plantsFilter
+      case 13: //enter
+        if (onInput) gInput.setFilterToFirstPlant();
+        break;
 
-		document.addEventListener('keydown', onKeyDown, false)
-	}
+      case 38: //up arrow
+        moveFocus(-4, e);
+        break;
+      case 40: //down arrow
+        moveFocus(4, e);
+        break;
+      case 37: //left arrow
+        if (!onInput) moveFocus(-1, e);
+        break;
+      case 39: //right arrow
+        if (!onInput) moveFocus(1, e);
+        break;
 
-	//TODO: INPUT AND FILTER EVENTS
-	function onKeyDown(e) {
-		const onInput = e.target.tagName.toLowerCase() === 'input'
+      default:
+        return true;
+    }
+  }
 
+  function moveFocus(incr, e) {
+    e.preventDefault();
 
-		if (gPage === 'plant' && e.keyCode === 27) router.navigate('🌿')
-		if (gPage === 'plant' && e.keyCode === 8) window.history.back()
-		if (gPage !== 'plants') return true
+    let pos = 0;
+    const lis = gInput.list.querySelectorAll("li:not([hidden])");
+    const activeLiPos = [].indexOf.call(
+      lis,
+      document.activeElement.parentElement
+    );
 
-		if (
-			!onInput &&
-			e.keyCode >= 65 &&
-			e.keyCode <= 90 &&
-			!e.altKey &&
-			!e.ctrlKey &&
-			!e.metaKey
-		) {
-			gInput.input.focus()
-			return true
-		}
-
-		switch (e.keyCode) {
-			case 27: //esc
-				gInput.setFilter('')
-				if (gInput.input) gInput.input.blur()
-				break
-
-			case 13: //enter
-				if (onInput) gInput.setFilterToFirstPlant()
-				break
-
-			case 38: //up arrow
-				moveFocus(-4, e)
-				break
-			case 40: //down arrow
-				moveFocus(4, e)
-				break
-			case 37: //left arrow
-				if (!onInput) moveFocus(-1, e)
-				break
-			case 39: //right arrow
-				if (!onInput) moveFocus(1, e)
-				break
-
-			default:
-				return true
-		}
-	}
-
-	function moveFocus(incr, e) {
-		e.preventDefault()
-
-		let pos = 0
-		const lis = gInput.list.querySelectorAll('li:not([hidden])')
-		const activeLiPos = [].indexOf.call(lis, document.activeElement.parentElement)
-
-		if (activeLiPos > -1) {
-			pos = incr + activeLiPos
-		}
-		if (pos < 0) {
-			gInput.input.focus()
-			return
-		}
-		if (pos > lis.length - 1) pos = lis.length - 1
-		lis[pos].firstElementChild.focus()
-	}
-}//Events
+    if (activeLiPos > -1) {
+      pos = incr + activeLiPos;
+    }
+    if (pos < 0) {
+      gInput.input.focus();
+      return;
+    }
+    if (pos > lis.length - 1) pos = lis.length - 1;
+    lis[pos].firstElementChild.focus();
+  }
+} //Events
--- prerender.config.js
+++ prerender.config.js
@@ -1,43 +1,40 @@
+require("./data/lang/en/plants");
+const en_plants = gPlantDataLang;
+require("./data/lang/de/plants");
+const de_plants = gPlantDataLang;
 
-require('./data/lang/en/plants');
-const en_plants = gPlantDataLang
-require('./data/lang/de/plants');
-const de_plants = gPlantDataLang
-
-let routesArray = ["/", "/🌿", "/de/", "/de/🌿"]
+let routesArray = ["/", "/🌿", "/de/", "/de/🌿"];
 
 for (var id in en_plants) {
-  routesArray.push('/🌿/' + id)
+  routesArray.push("/🌿/" + id);
 }
 
 for (var id in de_plants) {
-  routesArray.push('/de/🌿/' + toSlug(de_plants[id].name))
+  routesArray.push("/de/🌿/" + toSlug(de_plants[id].name));
 }
 
-console.log(" " + routesArray.length + " paths")
+// console.log(" " + routesArray.length + " paths")
 
 module.exports = {
-  "routes": routesArray,
-  "rendererConfig": {
+  routes: routesArray,
+  rendererConfig: {
     // "headless": false,
-    "renderAfterDocumentEvent": "prerender-trigger",
-    "maxConcurrentRoutes": 5,
-  }
-}
-
-
+    renderAfterDocumentEvent: "prerender-trigger",
+    maxConcurrentRoutes: 5,
+  },
+};
 
 function toSlug(v) {
-  v = v.toLowerCase()
-  v = v.replace(/\u00e4/g, 'ae')
-  v = v.replace(/\u00f6/g, 'oe')
-  v = v.replace(/\u00fc/g, 'ue')
-  v = v.replace(/\u00df/g, 'ss')
-  v = v.replace(/ /g, '-')
-  v = v.replace(/\./g, '')
-  v = v.replace(/,/g, '')
-  v = v.replace(/\(/g, '')
-  v = v.replace(/\)/g, '')
-  v = encodeURIComponent(v)
-  return v
+  v = v.toLowerCase();
+  v = v.replace(/\u00e4/g, "ae");
+  v = v.replace(/\u00f6/g, "oe");
+  v = v.replace(/\u00fc/g, "ue");
+  v = v.replace(/\u00df/g, "ss");
+  v = v.replace(/ /g, "-");
+  v = v.replace(/\./g, "");
+  v = v.replace(/,/g, "");
+  v = v.replace(/\(/g, "");
+  v = v.replace(/\)/g, "");
+  v = encodeURIComponent(v);
+  return v;
 }
